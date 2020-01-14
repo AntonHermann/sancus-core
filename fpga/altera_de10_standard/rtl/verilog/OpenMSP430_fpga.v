f577c876f8527b5ec70a40cd568ef05adc3db8c2
raup90 <raup90@zedat.fu-berlin.de>
2020-01-14 18:34:25 +0100

    add UART from HPS

--- fpga/altera_de10_standard/rtl/verilog/OpenMSP430_fpga.v
+++ fpga/altera_de10_standard/rtl/verilog/OpenMSP430_fpga.v
@@ -86,6 +86,67 @@ module DE10_Standard (
 	input 		          		IRDA_RXD,
 	output		          		IRDA_TXD,
 
+//	//////////// HPS //////////
+//	inout 		          		HPS_CONV_USB_N,
+//	output		    [14:0]		HPS_DDR3_ADDR,
+//	output		     [2:0]		HPS_DDR3_BA,
+//	output		          		HPS_DDR3_CAS_N,
+//	output		          		HPS_DDR3_CKE,
+//	output		          		HPS_DDR3_CK_N,
+//	output		          		HPS_DDR3_CK_P,
+//	output		          		HPS_DDR3_CS_N,
+//	output		     [3:0]		HPS_DDR3_DM,
+//	inout 		    [31:0]		HPS_DDR3_DQ,
+//	inout 		     [3:0]		HPS_DDR3_DQS_N,
+//	inout 		     [3:0]		HPS_DDR3_DQS_P,
+//	output		          		HPS_DDR3_ODT,
+//	output		          		HPS_DDR3_RAS_N,
+//	output		          		HPS_DDR3_RESET_N,
+//	input 		          		HPS_DDR3_RZQ,
+//	output		          		HPS_DDR3_WE_N,
+//	output		          		HPS_ENET_GTX_CLK,
+//	inout 		          		HPS_ENET_INT_N,
+//	output		          		HPS_ENET_MDC,
+//	inout 		          		HPS_ENET_MDIO,
+//	input 		          		HPS_ENET_RX_CLK,
+//	input 		     [3:0]		HPS_ENET_RX_DATA,
+//	input 		          		HPS_ENET_RX_DV,
+//	output		     [3:0]		HPS_ENET_TX_DATA,
+//	output		          		HPS_ENET_TX_EN,
+//	inout 		     [3:0]		HPS_FLASH_DATA,
+//	output		          		HPS_FLASH_DCLK,
+//	output		          		HPS_FLASH_NCSO,
+//	inout 		          		HPS_GSENSOR_INT,
+//	inout 		          		HPS_I2C1_SCLK,
+//	inout 		          		HPS_I2C1_SDAT,
+//	inout 		          		HPS_I2C2_SCLK,
+//	inout 		          		HPS_I2C2_SDAT,
+//	inout 		          		HPS_I2C_CONTROL,
+//	inout 		          		HPS_KEY,
+//	inout 		          		HPS_LCM_BK,
+//	inout 		          		HPS_LCM_D_C,
+//	inout 		          		HPS_LCM_RST_N,
+//	output		          		HPS_LCM_SPIM_CLK,
+//	input 		          		HPS_LCM_SPIM_MISO,
+//	output		          		HPS_LCM_SPIM_MOSI,
+//	output		          		HPS_LCM_SPIM_SS,
+//	inout 		          		HPS_LED,
+//	inout 		          		HPS_LTC_GPIO,
+//	output		          		HPS_SD_CLK,
+//	inout 		          		HPS_SD_CMD,
+//	inout 		     [3:0]		HPS_SD_DATA,
+//	output		          		HPS_SPIM_CLK,
+//	input 		          		HPS_SPIM_MISO,
+//	output		          		HPS_SPIM_MOSI,
+//	output		          		HPS_SPIM_SS,
+	input 		          		HPS_UART_RX,
+	output		          		HPS_UART_TX,
+//	input 		          		HPS_USB_CLKOUT,
+//	inout 		     [7:0]		HPS_USB_DATA,
+//	input 		          		HPS_USB_DIR,
+//	input 		          		HPS_USB_NXT,
+//	output		          		HPS_USB_STP,
+
 	//////////// GPIO, GPIO connect to GPIO Default //////////
 	inout 		    [35:0]		GPIO
 );
@@ -513,9 +574,8 @@ assign p2_io_din[2] = p3_din[0] ? 1'b1         : uart_rxd_in;
 assign dbg_uart_rxd = p3_din[0] ? uart_rxd_in  : 1'b1;
 
 
-// assign uart_rxd_in = UART_RXD;
-assign uart_rxd_in = 1'b1; // NEW
-assign UART_TXD = uart_txd_out;
+assign uart_rxd_in = HPS_UART_RX;
+assign HPS_UART_TX = uart_txd_out;
 
 
 
